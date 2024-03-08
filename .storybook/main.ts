d9793abbec0df1fe63fd7bf5d59756eed4304d8d
Big Daddy <craigedney@britishcycling.org.uk>
2024-03-08 15:52:56 +0000

    Storybook added

---
+++ .storybook/main.ts
@@ -0,0 +1,30 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+import path from "node:path";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  webpackFinal: (config) => {
+    // Ensure config.resolve object exists
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src/"), // Adjust the path as necessary
+    };
+
+    return config;
+  },
+};
+export default config;
